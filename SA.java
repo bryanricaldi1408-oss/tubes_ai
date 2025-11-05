db7c4810f5a128e2fb1a6bdab9c3267b3094fc06
IEatSteak <vin24farrel@gmail.com>
2025-11-05 22:56:53 +0700

    Delete SA.java

--- SA.java
+++
@@ -1,8 +0,0 @@
-public class SA {
-    static double simulatedAnnealing(double t0, double cooling, double stopping_temp, double stepSize) {
-        return 0;
-    }
-    public static void main(String[] args) {
-        
-    }
-}
