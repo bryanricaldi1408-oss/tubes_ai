396c9dac8575f6907c62eaeb63a1848b5858c923
Bryan Ricaldi <bryan>
2025-11-08 18:00:59 +0700

    buat constructor untuk test case

--- TestCaseGenerator.java
+++ TestCaseGenerator.java
@@ -1,5 +1,28 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class TestCaseGenerator {
-    public static void main(String[] args) {
+    private final int m, n, p, h, t;
+    private final Random rnd;
+
+    public TestCaseGenerator(int m, int n){
+        this.m = m;
+        this.n = n;
+        this.rnd = new Random();
+        int totalCells = m*n;
+
+        double housePercent = 0.15 + (rnd.nextDouble() * 0.05);
+        this.h = (int) (housePercent * totalCells);
         
+        double treePercent = 0.20 + (rnd.nextDouble() * 0.05);
+        this.t = (int) (treePercent * totalCells);
+
+        double housePerStation= 15 + (rnd.nextInt(11));
+        this.p = (int) Math.ceil((double) this.h/housePerStation);
+        
+        if(h + t > totalCells){
+            System.err.println("Jumlah rumah dan pohon melebihi total set.");
+        }
     }
+    
 }
